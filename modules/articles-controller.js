5e506dc78ff8da61f5d378cb9c0cf8fab406beb0
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-05 17:02:44 +0000

    Created controller for articles only

---
+++ modules/articles-controller.js
@@ -0,0 +1,41 @@
+const database_url = "mongodb://localhost:27017/yummy_articles"
+const articles_collection = "articles"
+
+const database = require('./articles-db')
+
+// Function to add a new article
+exports.add = function(articleObject, callback){
+    database.addResourceToCollection(database_url, articles_collection, articleObject)
+    callback()
+};
+
+// Function to retrieve one article
+exports.getById = function(articleId, callback){
+    database.getResourceFromCollection(database_url, articles_collection, articleId, function(article) {
+        return callback(article)
+    })
+};
+
+// Function to retrieve all articles
+exports.getAll = function(err, callback){
+    // If there's an error from the function call, exit with error message
+    if (err) throw err;
+
+    // Call database to add the new article record to the article collection
+    // Once done, pass the articles_list result as the parameter to the callback function
+    database.getAllFromCollection(database_url, articles_collection, function(articles_list) {
+        return callback(articles_list)
+    })
+};
+
+// Function to update a article
+exports.update = function(articleID, newarticleDetailsObject, callback){
+    database.updateResource(database_url, articles_collection, articleID, newarticleDetailsObject)
+    callback()
+};
+
+// Function to delete a article
+exports.delete = function(articleID, callback){
+    database.deleteResource(database_url, articles_collection, articleID)
+    callback() 
+};
