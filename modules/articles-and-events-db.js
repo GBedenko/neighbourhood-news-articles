8efffcf59457639d156743a36f1962c01d022bb8
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-05 16:50:21 +0000

    Created db interactions module

---
+++ modules/articles-and-events-db.js
@@ -0,0 +1,24 @@
+// Import package for mongodb client
+const MongoClient = require('mongodb').MongoClient
+const mongodb = require('mongodb')
+
+
+exports.addResourceToCollection = function() {
+    // Add one resource to provided collection
+}
+
+exports.getAllFromCollection = function() {
+    // Retrieve all resources from a given collection
+}
+
+exports.getResourceFromCollection = function() {
+    // Retrieve a specific resource from a collection
+}
+
+exports.updateResource = function() {
+    // Update the given resource
+}
+
+exports.deleteResource = function(database_url, collection_name, articleID) {
+    // Delete the given resource
+}
