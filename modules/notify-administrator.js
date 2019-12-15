e348f8261934d95ececbb87ba92683e083cfa202
Genaro Bedenko <genaro.bedenko@gmail.com>
2019-12-15 21:06:58 +0000

    Restructure project files

--- modules/notify-administrator.js
+++ modules/notify-administrator.js
@@ -5,16 +5,16 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user: 'bedenkog.local.news@gmail.com',
-		pass: 'localnews-temp'
+		user: '',
+		pass: ''
 	}
 })
 
 exports.emailAdministrator = () => {
 
 	const mailOptions = {
-		from: 'bedenkog.local.news@gmail.com',
-		to: 'genaro.bedenko@outlook.com',
+		from: '',
+		to: '',
 		subject: 'New post has been created on the Local News Application!',
 		text: 'Please check the Local News Application and approve the new content someone created: http://localhost:8080/admin_dashboard'
 	}
